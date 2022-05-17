4e932da9f6e65a8db1fd53b21bf0badad28ffa0e
Ben Whelan <bmwhelan@yahoo.ca>
2022-05-17 14:52:42 -0400

    Added column strings manager class

---
+++ FrontEnd/ColumnStringsManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontEnd
+{
+    class ColumnStringsManager
+    {
+        public ColumnStringsManager()
+        {
+
+        }
+
+        public string PostCall(string postCallInput)
+        {
+
+        }
+
+        public string OnCall(string OnCallInput)
+        {
+
+        }
+
+        public string StaffName(string StaffNameInput)
+        {
+
+        }
+
+        public string Assignment(string assignmentInput)
+        {
+
+        }
+
+        public string Notes(string notesInput)
+        {
+
+        }
+    }
+}
